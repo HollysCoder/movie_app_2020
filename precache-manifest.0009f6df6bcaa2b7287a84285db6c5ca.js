c73c22350e7709bca03cb7f417c95e8becb18c4f
HollysCode <quddlr9015@naver.com>
2020-07-21 18:21:23 +0900

    Updates

---
+++ precache-manifest.0009f6df6bcaa2b7287a84285db6c5ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc0d158161825930ed84070a4589187",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a1cda1ad7f0ae9051321",
+    "url": "/movie_app_2020/static/css/main.c569aab2.chunk.css"
+  },
+  {
+    "revision": "7184ce8aadc9b9c078da",
+    "url": "/movie_app_2020/static/js/2.427047e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.427047e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1cda1ad7f0ae9051321",
+    "url": "/movie_app_2020/static/js/main.94ef1526.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
